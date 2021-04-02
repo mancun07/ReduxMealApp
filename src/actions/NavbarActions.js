ee7b2bdc3a12bc0cacb9493c3091b76a9d9de371
Dima <address>
2021-04-02 08:23:48 +0300

    XXX

--- src/actions/NavbarActions.js
+++ src/actions/NavbarActions.js
@@ -1,12 +1,9 @@
-import {TOGGLE_MENU, CHANGE_NAVBAR_COLOR} from './types'
+import { TOGGLE_MENU, CHANGE_NAVBAR_COLOR } from "./types";
 
-export const toggleMenu = () => dispatch => {
-    dispatch({type: TOGGLE_MENU})   
-}
+export const toggleMenu = () => (dispatch) => {
+  dispatch({ type: TOGGLE_MENU });
+};
 
-
-export const changeNavbarColor = () => dispatch => {
-    console.log('Hello from changeNavbarColor')
-    dispatch({type: CHANGE_NAVBAR_COLOR})   
-}
-
+export const changeNavbarColor = () => (dispatch) => {
+  dispatch({ type: CHANGE_NAVBAR_COLOR });
+};
